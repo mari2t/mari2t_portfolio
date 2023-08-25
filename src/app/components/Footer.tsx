2d30d9e9c1f7019a7fad02d28a3a31abcdc5b9b3
mari2t <subcodeoft@gmail.com>
2023-08-25 21:01:51 +0900

    style : Githubロゴ追加

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="bg-white pt-2 sm:pt-4 lg:pt-8">
       <footer className="mx-auto max-w-screen-2xl px-2 md:px-4">
         <div className="flex flex-col items-center border-t pt-6">
-          {/* nav - start */}
           <nav className="mb-4 flex flex-wrap justify-center gap-x-4 gap-y-2 md:justify-start md:gap-6">
             <div className="flex space-x-4">
               <Link href="/" className="text-stone-600 font-bold text-xl">
@@ -16,6 +16,12 @@ const Footer = () => {
               </Link>
               <Link href="/about" className="text-stone-600 font-bold text-xl">
                 About
+              </Link>
+              <Link
+                href="https://github.com/mari2t"
+                className="text-stone-600 font-bold text-xl"
+              >
+                <FaGithub />
               </Link>
             </div>
           </nav>
