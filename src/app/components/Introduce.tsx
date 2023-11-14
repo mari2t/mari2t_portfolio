3144fe9caf40acdd584f456bda4be9a315a354ab
mari2t <subcodeoft@gmail.com>
2023-11-14 18:39:14 +0900

    fix : レスポンシブ対応

--- src/app/components/Introduce.tsx
+++ src/app/components/Introduce.tsx
@@ -9,8 +9,8 @@ const Introduce = () => {
           alt="プロフィール画像"
           className="h-24 w-24 rounded-full border-4 mr-4"
         />
-        <div className="chat chat-start w-2/5 flex items-end mb-4">
-          <div className="chat-bubble">
+        <div className="chat chat-start w-4/5 items-end mb-4 lg:w-2/5">
+          <div className="chat-bubble text-sm lg:text-base ">
             こんにちは、mari2tです。 <br />
             web関係を勉強中です。現在は東京在住の技術職です。 <br />
             ここには作ったものやスキル等を載せています。
