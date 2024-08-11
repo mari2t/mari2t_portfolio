39213b80aefa585554252503f76ded1d62a20a5d
mari2t <subcodeoft@gmail.com>
2024-08-11 12:41:41 +0900

    chore : style,About

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,20 +4,20 @@ import { FaGithub } from "react-icons/fa";
 
 const Header: FunctionComponent = () => {
   return (
-    <header className="flex justify-between p-4 bg-lime-400">
+    <header className="flex justify-between p-4 bg-cyan-600">
       <div>
-        <Link href="/" className="text-stone-600 font-bold text-3xl">
+        <Link href="/" className="text-cyan-100 font-bold text-3xl">
           mari2t Portfolio Site
         </Link>
       </div>
       <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
-        <Link href="/" className="text-stone-600 font-bold text-xl">
+        <Link href="/" className="text-cyan-100 font-bold text-xl">
           Portfolios
         </Link>
-        <Link href="/skills" className="text-stone-600 font-bold text-xl">
+        <Link href="/skills" className="text-cyan-100 font-bold text-xl">
           Skills
         </Link>
-        <Link href="/about" className="text-stone-600 font-bold text-xl">
+        <Link href="/about" className="text-cyan-100 font-bold text-xl">
           About
         </Link>
       </div>
